90ce8abc3e47998ab37cbf9ae78a0aa2b5436949
David-Jesse <djesse351@gmail.com>
2026-07-05 12:22:28 +0100

    Made changes to the service hero page

--- feg/src/components/resources/Hero.tsx
+++ feg/src/components/resources/Hero.tsx
@@ -1,5 +1,5 @@
 import { Button } from "../../ui/Button";
-import resourcesHero from "../../assets/resources/resourcesHero.jpg";
+import resourcesHero from "../../assets/resources/resourcesHero.jpg"
 
 export default function Hero() {
     return (
